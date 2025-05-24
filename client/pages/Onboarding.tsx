5d9708cca2e26df36d07d2452531593b4005f279
Wahid Hoque <wahidhoquee@gmail.com>
2025-05-25 02:14:24 +0600

    feat: tailwind and shadcn setup

---
+++ client/pages/Onboarding.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Onboarding = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-red-400">Onboarding</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Onboarding;
